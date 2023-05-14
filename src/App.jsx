241cde6e1a77c49a180fede914144d2d35df6817
vishek.patel <vishek.patel@moveinsync.com>
2023-05-14 22:32:14 +0530

    css changed

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ const App = () => {
           </div>
         )}
       </div>
-      <div className="author">Made by - Faishal Saif</div>
+      <div className="author">Made by - Vishek Patel</div>
     </div>
   );
 };
