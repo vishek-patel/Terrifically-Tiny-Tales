86d509cbd477a905185c3df427692e75ab5b99e5
Vishek Patel <85006315+vishek-patel@users.noreply.github.com>
2023-05-14 16:03:03 +0530

    Update chart.jsx

--- src/chart.jsx
+++ src/chart.jsx
@@ -52,7 +52,7 @@ const options = {
     }
   },
   series: [{
-    name: 'Frequency',
+    name: 'Words',
     data: [27, 23, 21, 15, 13, 13, 13, 11, 10, 10, 10, 9, 9, 8, 8, 8, 7, 6, 6, 6]
   }]
 };
